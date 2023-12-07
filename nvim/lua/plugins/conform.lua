c57766f137eabd6beebb304f5d93c59d89370ec6
Liam <lbrown@rkh.co.uk>
2023-12-07 12:30:39 +0200

    use correct command for ruff formatting

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,10 +1,10 @@
 return {
   "stevearc/conform.nvim",
-  optional = true,
+  -- optional = true,
   opts = {
     formatters_by_ft = {
       markdown = { { "prettierd", "prettier" } },
-      python = { "ruff" },
+      python = { "ruff_fmt" },
       htmldjango = { "djlint" },
       svelte = { { "prettierd", "prettier" } },
     },
