1b7243342618880f754ec5c28b34c6b0a74fe759
Liam <lbrown@rkh.co.uk>
2024-06-10 13:25:26 +0300

    use Trouble to auto open some useful panels

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,11 +2,11 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = "*",
-  callback = function()
-    vim.cmd("Trouble symbols open focus=false")
-    vim.cmd("Trouble lsp open focus=false win.position=right")
-    vim.cmd("Trouble diagnostics open filter.buf=0")
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufReadPost", {
+--   pattern = "*",
+--   callback = function()
+--     vim.cmd("Trouble symbols open focus=false")
+--     vim.cmd("Trouble lsp open focus=false win.position=right")
+--     vim.cmd("Trouble diagnostics open filter.buf=0")
+--   end,
+-- })
--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,9 +1,46 @@
 return {
   "folke/trouble.nvim",
-  branch = "dev",
   -- opts will be merged with the parent spec
   opts = {
-    auto_open = true,
-    auto_close = true,
+    modes = {
+      current_buffer_diagnostics = {
+        auto_open = true,
+        mode = "diagnostics",
+        win = { position = "bottom" },
+        filter = { buf = 0 },
+      },
+      symbols = {
+        desc = "document symbols",
+        mode = "lsp_document_symbols",
+        focus = false,
+        auto_open = true,
+        win = { position = "right" },
+        filter = {
+          -- remove Package since luals uses it for control flow structures
+          ["not"] = { ft = "lua", kind = "Package" },
+          any = {
+            -- all symbol kinds for help / markdown files
+            ft = { "help", "markdown" },
+            -- default set of symbol kinds
+            kind = {
+              "Class",
+              "Constructor",
+              "Enum",
+              "Field",
+              "Function",
+              "Interface",
+              "Method",
+              "Module",
+              "Namespace",
+              "Package",
+              "Property",
+              "Struct",
+              "Trait",
+            },
+          },
+        },
+      },
+      lsp = { auto_open = true, win = { position = "right" }, focus = false },
+    },
   },
 }
